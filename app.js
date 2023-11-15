dec72c5f4fdec5745b3699990fbd87ec1adeba53
Ian-Ban <146909259+Ian-Ban@users.noreply.github.com>
2023-11-15 00:14:58 -0500

    Fixed issue with not connecting to localhost:3000

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-// app.js
 const express = require('express');
-const initializeDatabase = require('./database'); 
+const initializeDatabase = require('./database');
 const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
 app.use(express.json());
-app.use('/users', userRoutes); 
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to LearnLink!');
+});
+
+
+app.use('/users', userRoutes);
 
 const PORT = 3000;
+
 
 initializeDatabase().then(() => {
   app.listen(PORT, () => {
