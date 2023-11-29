c1492531fd4bd2d7bd847f373b565d40dffda961
chris-codes-programs <epicnintendogamer500@gmail.com>
2023-11-29 18:08:39 -0500

    My commit
    I did at least most of question 1 if not all of it, and part of Question 2.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,4 +6,22 @@ const { createUser } = require('../controllers/user.controller');
 
 router.post('/', createUser);
 
+router.get('/signin', function(req, res) {
+    res.render('signin', {title: "Signin"})
+})
+
+router.get('/', function(req, res) {
+    res.render('home', {title: "Home"})
+})
+
+router.get('/', function(req, res) {
+    res.render('home', {
+        home: users
+    })
+})
+
+router.get('/signup', function(req, res) {
+    res.render('signup', {title: "Signup"})
+})
+
 module.exports = router;
