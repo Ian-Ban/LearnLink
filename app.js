ce0d3cfd60332ff1af64bfbbc275828c1eb814eb
TaylorTWBrown <taylorami@hotmail.com>
2023-12-13 19:20:16 -0500

    fix images and css

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-// app.js
 const express = require('express');
 const initializeDatabase = require('./database');
 const userRoutes = require('./routes/user.routes');
 const authRoutes = require('./routes/auth.routes');
-var path = require('path');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' folder
 
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.render('home', {title: "Home"});
+  res.render('home', { title: 'Home' });
 });
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
 const PORT = 3000;
-
-//users/signup
-//users/signin
-
-
 
 initializeDatabase().then(() => {
   app.listen(PORT, () => {
